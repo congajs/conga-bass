e1af37a3ec356f48cb353fe146cf890a5f2d3197
Anthony Matarazzo <anthony.matarazzo@neustar.biz>
2017-08-17 11:48:17 -0700

    fix entry point; verbose error

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
+const Bass = require('bass');
+
 module.exports = {
 
-    AbstractFixture: require('./lib/fixture/AbstractFixture') ,
-    
-	Bass: {
-
-
-
-		Module: require('bass') ,
-
-		Query: require('./node_modules/bass/lib/query') ,
-
-		QueryResult: require('./node_modules/bass/lib/query-result') ,
-
-		Manager: {
-			Definition: require('./node_modules/bass/lib/manager-definition') ,
-			Factory: require('./node_modules/bass/lib/manager-factory') ,
-			Manager: require('./node_modules/bass/lib/manager')
-		} ,
-
-		Registry: require('./node_modules/bass/lib/registry') ,
-
-		DocumentCache: require('./node_modules/bass/lib/document-cache') ,
-
-		Mapper: require('./node_modules/bass/lib/mapper') ,
-
-		UnitOfWork: require('./node_modules/bass/lib/unit-of-work')
-	}
+    AbstractFixture: require('./lib/fixture/AbstractFixture')
 
 };
